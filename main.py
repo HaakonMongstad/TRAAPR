568eb965dff7ab04a49f692bf209df4a9cf9d51b
HaakonMongstad <haakon@mongstad.net>
2023-02-03 18:52:02 -0600

    -Updated file structure -began to implement first RL agent

--- main.py
+++ main.py
@@ -8,12 +8,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import matplotlib.animation as animation
-import abc
 from numpy.typing import NDArray
-import gymnasium as gym
 from scipy import signal
 
-VISON_SIZE = 5
+import Agents
+import Learning
 
 fig = plt.figure(figsize=(6,6))
 
@@ -39,108 +38,6 @@ def anim_builder(frames):
   anim = animation.FuncAnimation(fig, frame, frames=frames, fargs = [ax], blit=True, repeat=True)
   plt.show()
   return anim
-
-class Agent():
-  __metaclass__ = abc.ABCMeta
-
-  def __init__(self,
-                 obs_onehot: bool = False,
-                 attack_map: bool = False,
-                 attack_map_logits: bool = False,
-                 ):
-        """
-        :param obs_onehot: Determines whether the observation that is received is one-hot encoded or not
-        :type obs_onehot: bool
-        :param attack_map: Determines whether you pass in a probability map for each cell's attack
-        (4 dimensional array for your total action)
-        :type attack_map: bool
-        :param attack_map_logits: If you pass in a map for each cell's attack, this determines whether it
-        is a logit or probability map
-        :type attack_map_logits: bool
-        """
-        self.obs_onehot = obs_onehot
-        self.attack_map = attack_map
-        self.attack_map_logits = attack_map_logits
-
-  @abc.abstractmethod
-  def policy(self, obs: NDArray, action_space, obs_space) -> NDArray:
-
-      """
-      :param obs: height*width array of game_state
-      :action_space: unused - from AI gym in python project version
-      :obs_space: unused - from AI gym in python project version
-      """
-      raise NotImplementedError("Please implement the policy method!")
-
-
-
-
-
-      ### WRITTEN ENEMY AGENTS ###
-
-class dummy_agent(Agent):
-  def policy(self, state_mat, **kwargs):
-    h,w = state_mat.shape
-    attack_mat = np.random.randint(0,25,(h,w)) #get cell feats returns adjacent cells
-
-    return attack_mat.astype(int)
-
-class ReinforcementLearningAgent(Agent):
-  
-  
-  def adjacent_cells(self, state_mat, x, y):
-    """
-    
-    """
-    w, h = state_mat.shape
-
-    rowIndices, colIndices = np.indices((VISON_SIZE, VISON_SIZE))
-    rowIndices = (rowIndices + x - (VISON_SIZE/2)) % h
-    colIndices = (colIndices + y - (VISON_SIZE/2)) % w
-
-    adj_cells = state_mat[rowIndices, colIndices]
-    return adj_cells
-
-  def policy(self, state_mat, **kwargs):
-    pass
-
-class first_enemy_agent(Agent):
-
-  def simple_adj(self, feat_mat):
-    feat_mat = feat_mat.reshape(-1,1)
-
-    enemy_list = np.argwhere(feat_mat >= 3)[:,0]
-    empty_list = np.argwhere(feat_mat == 0)[:,0]
-
-    if len(enemy_list)>=1: #if there is at least one enemy
-      return enemy_list[0]
-    elif len(empty_list)>=1: #if there is at least one empty
-      return empty_list[0]
-    else:
-      return 12
-    
-  #get the adjacent features for a given x,y cell
-  def get_cell_feats(self,state_mat, x,y, n = 2):
-    w,h = state_mat.shape
-    adj_width = 2*n+1
-
-    xind,yind = np.indices((adj_width,adj_width))
-    xind = (xind -n +x)%h
-    yind = (yind -n +y)%w
-
-    #gets a matrix of the adjacent cells by 1
-    adj_vals = state_mat[xind,yind]
-    return adj_vals
-
-  def policy(self, state_mat, **kwargs):
-    attack_mat =np.zeros(state_mat.shape)
-
-    for i in range(attack_mat.shape[0]):
-      for j in range(attack_mat.shape[1]):
-        attack_mat[i,j] = self.simple_adj(self.get_cell_feats(state_mat, i,j,2)) #get cell feats returns adjacent cells
-
-    return attack_mat.astype(int)
-
 
 class cgol:
   def  __init__(self, agent_1, agent_2, board_h = 10, board_w = 10, attack_width = 5, map = None):
@@ -319,17 +216,32 @@ class cgol:
   def run_game(self, n_steps = 10):
     state_mats = [self.step(return_state = True) for i in range(n_steps)]
     return state_mats
+
+
+#def train_model(model):
   
 
 
-### TEST GAME ###
-agent_1 = first_enemy_agent()
-agent_2 = dummy_agent()
+def main():
 
-#walls = np.random.binomial(1,0.2, (10,10))
 
-test_game = cgol(agent_1, agent_2,10,10)
+  # model = Learning.get_model()
+  # model.summary()
+  # Learning.build_agent(model)
 
-game_hist = test_game.run_game(30)
 
-anim_builder(game_hist)
+
+  agent_1 = Agents.first_enemy_agent()
+  agent_2 = Agents.dummy_agent()
+
+  #walls = np.random.binomial(1,0.2, (10,10))
+
+  test_game = cgol(agent_1, agent_2, 10,10)
+
+  game_hist = test_game.run_game(30)
+
+  anim_builder(game_hist)
+
+
+if __name__ == "__main__":
+  main()
